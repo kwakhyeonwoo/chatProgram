083f8f6ed34638be153dfc24bc2827e38688d529
곽현우 <polla723@naver.com>
2024-09-10 15:05:17 +0900

    Initial Commit

---
+++ chatProgram/chatProgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatProgramApp.swift
+//  chatProgram
+//
+//  Created by 곽현우 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct chatProgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
